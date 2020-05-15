e41e3348be3082ba2a3c283e34cd255c920521c4
J-A-C-E <J-A-C-E@users.noreply.github.com>
2020-05-15 01:21:18 -0500

    Se sube el proyecto
    Se agrega el proyecto de google drive, se realiza la conexion al server.
    
    No logra verificar la contraseña de inicio de sesión

---
+++ Proyecto-Cerrajeria-master-20200515T053510Z-001/Proyecto-Cerrajeria-master/Cerrajeria 2/Cerrajeria 2/Conexion_BDCerrajeria.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Cerrajeria_2
+{
+    class Conexion_BDCerrajeria
+    {
+        SqlConnection conexion;
+        SqlCommand command;
+        SqlDataReader dataRead;
+        SqlDataAdapter dataAdap;
+        DataTable dataTab;
+
+        //EL DE MARI
+        //SqlConnection con = new SqlConnection("Data Source=LAPTOP-DTRPMJU7;Initial Catalog=Cerrajeria_Hns;Integrated Security=True");
+
+        //EL NUEVO
+        SqlConnection con = new SqlConnection("Data Source=34.71.252.51;Initial Catalog=Cerrajeria_Hns;Persist Security Info=True;User ID=sqlserver;Password=123");
+
+        public Conexion_BDCerrajeria()
+        {
+            try
+            {
+                conexion = new SqlConnection("Data Source=LAPTOP-DTRPMJU7;Initial Catalog=Cerrajeria_Hns;Integrated Security=True");
+                conexion.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se pudo establecer la conexión\n" + ex.ToString());
+                conexion.Close();
+            }
+        }
+
+    }
+}
